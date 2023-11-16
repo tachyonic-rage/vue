ff6692ad2bfbd22497be886681e45ffcbcb8a6d0
Tachyonic Grace <56035952+tachyonic-rage@users.noreply.github.com>
2023-11-16 11:39:27 +0000

    Update 8 files

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "daniel",
+          name: "Prophet Daniel",
+          phone: "012345678900",
+          email: " prophetdaniel@netcall.com",
+        },
+        {
+          id: "siloaun",
+          name: "St Siloaun",
+          phone: "012345678900",
+          email: " prophetdaniel@netcall.com",
+        },
+      ],
+    };
+  },
+};
+</script>
